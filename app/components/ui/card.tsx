57e295c8e24146f49740f73979bffa55de06839a
SteveDSF1283 <s.ptg91@gmail.com>
2026-04-14 02:19:13 +0200

    Create card.tsx

---
+++ app/components/ui/card.tsx
@@ -0,0 +1,19 @@
+export function Card({
+  className = "",
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) {
+  return <div className={className}>{children}</div>;
+}
+
+export function CardContent({
+  className = "",
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) {
+  return <div className={className}>{children}</div>;
+}
